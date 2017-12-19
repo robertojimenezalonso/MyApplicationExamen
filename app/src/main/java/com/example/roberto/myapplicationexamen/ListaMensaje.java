1e26ac5ca4d10e674149c64367ec1baabd5c189d
Roberto <robertojimenezalonso@gmail.com>
2017-12-19 14:47:45 +0100

    commit segundo

---
+++ app/src/main/java/com/example/roberto/myapplicationexamen/ListaMensaje.java
@@ -0,0 +1,47 @@
+package com.example.roberto.myapplicationexamen;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListaMensaje extends Fragment {
+
+    public RecyclerView recyclerView;
+
+
+    public ListaMensaje() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_lista_mensaje, container, false);
+        recyclerView=v.findViewById(R.id.listamensaje);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+
+        ArrayList<String> mdatos= new ArrayList<>();
+        mdatos.add("Noticia 1");
+        mdatos.add("Noticia 2");
+        mdatos.add("Noticia 3");
+        mdatos.add("Noticia 4");
+
+        ListaMensajeAdapter listaMensajeAdapter = new ListaMensajeAdapter(mdatos);
+        recyclerView.setAdapter(listaMensajeAdapter);
+        return v;
+    }
+
+}
---
+++ app/src/main/java/com/example/roberto/myapplicationexamen/ListaMensajeAdapter.java
@@ -0,0 +1,52 @@
+package com.example.roberto.myapplicationexamen;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by roberto on 19/12/17.
+ */
+
+public class ListaMensajeAdapter extends RecyclerView.Adapter<MensajeViewHolder> {
+
+    private ArrayList<String> mensaje;
+
+    public ListaMensajeAdapter(ArrayList<String> mensaje){
+        this.mensaje=mensaje;
+    }
+
+    @Override
+    public MensajeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.celda_mensaje_layout, null);
+    MensajeViewHolder mensajeViewHolder = new MensajeViewHolder(view);
+
+        return mensajeViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MensajeViewHolder holder, int position) {
+        holder.textoMensaje.setText(mensaje.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return mensaje.size();
+    }
+}
+
+class MensajeViewHolder extends RecyclerView.ViewHolder{
+
+    public TextView textoMensaje;
+
+
+    public MensajeViewHolder(View itemView) {
+        super(itemView);
+        textoMensaje=itemView.findViewById(R.id.textoMensaje);
+    }
+}
